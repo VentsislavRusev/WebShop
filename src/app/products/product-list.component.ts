2e6783a7951ccb7eddd4faec3069758e6b2ce3d9
Ventsislav Rusev <ventsirusev1@gmail.com>
2021-01-26 19:58:46 +0200

    Add functionality to the filter input field for the list of products
    Here I have created the functionality for filtering the list of products.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,21 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    
+    // For our filter I would like to create a new Iproduct[] array that holds
+    // our filtered products, without losing the actual products
+    _listFilter: string;
+    get listFilter() {
+      return this._listFilter;
+    }
+    set listFilter(value:string) {
+      this._listFilter = value;
+      // if the listFilter is empty it will display the list of products. If the listFilter is not empty it will perform the performFilter operation
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+
     products: IProduct[] = [
         {
           "productId": 1,
@@ -66,6 +80,24 @@ export class ProductListComponent implements OnInit {
         }
       ];
       
+      // we want to set default values for both the filteredProducts and the _listFilter properties
+      // the class constructor is a function that is executed when a component is first initialized
+      constructor() {
+        this.filteredProducts = this.products;
+        this._listFilter = 'cart';
+      }
+
+      // this code starts by converting the filter criteria to lowercase 
+      performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        // then we return the filtered list of products 
+        // we are using the array filter method to create a new array with elements that pass the test defined in the provided function 'filter'
+        return this.products.filter((product:IProduct) =>
+        // for each product in the list the product name is converted to lowercase 
+        //the indexOf is used to determine if the filter text is found in the product name
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+      }
+
       toggleImage(): void {
         this.showImage = !this.showImage;
       }
